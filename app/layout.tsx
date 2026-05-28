2ef4e3682a5ad42ba0e69eab4629c2f260c933cd
Stewart <brian.ngetich1@s.karu.ac.ke>
2026-05-28 23:36:53 +0300

    one more

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import WhatsAppButton from "@/Components/Ui/WhatsAppButton";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   metadataBase: new URL("https://your-project.vercel.app"),
   title: {
